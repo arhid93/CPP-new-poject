327ffbd5e0b4323619c4b738c0e546c61e855f32
Arom Hidayat <hidayatarom@gmail.com>
2017-03-20 09:53:26 +0700

    Add files via upload

---
+++ konstanta.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#define PHI 3.14 //konstanta
+
+using namespace std;
+int main() {
+	float jari_jari, luas;
+   //konstanta
+   jari_jari + 25;
+
+   luas = 0.5 * PHI * jari_jari * jari_jari;
+   cout<<"Data lingkaran";
+   cout<<"\njari-jari : "<<jari_jari;
+   cout<<"nluas : "<<cariluas;
+   cout<<"\n";
+   return 0;
+   }
---
+++ var.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int a, b;
+   int c;
+   float f;
+
+   a = 10;
+   b = 20;
+   c = a + b;
+   cout << c << endl ;
+   f = 70.0/3.0;
+   cout << f << endl ;
+   return 0;
+}
